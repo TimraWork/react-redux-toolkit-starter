f85c521fbacc69b4afa4728046cfe985366b49e1
Timra <59473768+TimraWork@users.noreply.github.com>
2021-03-27 15:10:45 +0500

    fix: safe language after page reload

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./en/translation.json";
 import ru from "./ru/translation.json";
@@ -13,10 +14,18 @@ export const resources = {
   },
 } as const;
 
-i18n.use(initReactI18next).init({
-  fallbackLng: "en",
-  debug: true,
-  resources,
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    resources,
+    keySeparator: false,
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
